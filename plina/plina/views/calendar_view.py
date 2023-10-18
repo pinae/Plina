99dc71d16b7439849bdd6dd5b4f47ca55826db35
pinae <pina@pinae.net>
2023-10-18 11:22:40 +0200

    Calendar Widget created

--- plina/plina/views/calendar_view.py
+++ plina/plina/views/calendar_view.py
@@ -1,9 +1,21 @@
+from lona.request import Request
+from lona.server import Server
 from lona.view import LonaView
-from lona.html import HTML, H1
+from lona.html import HTML
+from lona.view_runtime import ViewRuntime
+from lona_picocss.html import H1, Div
+from calendar_widget import CalendarWidget
 
 
 class CalendarView(LonaView):
-    def handle_request(self, request):
+    def __init__(self, server: Server, view_runtime: ViewRuntime, request: Request):
+        super().__init__(server, view_runtime, request)
+        self.calendar_days = [
+            CalendarWidget(),
+        ]
+
+    def handle_request(self, request: Request):
         return HTML(
             H1('Calendar'),
+            Div(self.calendar_days),
         )
---
+++ plina/plina/views/calendar_widget.py
@@ -0,0 +1,38 @@
+from lona_picocss.html import Div
+from lona.html import Node, CLICK
+from lona.static_files import StyleSheet, StaticFile
+
+
+class CalendarWidget(Node):
+    TAG_NAME = 'calendar-widget'
+
+    STATIC_FILES = [
+        StyleSheet(
+            name='calendar-widget',
+            path='../static/calendar-widget.css',
+        ),
+        StaticFile(
+            name='calendar-background.svg',
+            path='../static/calendar-background.svg',
+        ),
+    ]
+
+    CLASS_LIST = ['calendar-widget']
+    EVENTS = [CLICK]
+
+    def __init__(self, *args, height="1440px", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # setting up widget data
+        # will be available as `this.data` in the widget
+        self.widget_data = {
+            'height': height,
+            'tasks': [],
+        }
+
+        self.nodes = [
+            Div("Foo"),
+        ]
+
+    def set_tasks(self, task_list):
+        self.widget_data['tasks'] = task_list
